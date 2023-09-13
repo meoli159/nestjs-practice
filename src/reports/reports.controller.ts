3c60982dae032568b8717812f4edb92f5964d609
Cyrus <kur0dk0@gmail.com>
2023-09-14 01:10:04 +0700

    final code

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -12,13 +12,13 @@ import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
 import { CurrentUser } from '../users/decorators/current-user.decorator';
-import { User } from 'src/users/user.entity';
+import { User } from '../users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from '../guards/admin.guard';
 import { GetEstimateDto } from './dtos/get-estimate.dto';
-import { query } from 'express';
+
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
